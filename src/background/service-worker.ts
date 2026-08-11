0209714ec314c0fc5c56cdfaccd6431dd01add4f
Tridib Banik <tridib.perfect@gmail.com>
2026-08-10 22:49:34 -0400

    Wire service worker to open side panel on icon click

--- src/background/service-worker.ts
+++ src/background/service-worker.ts
@@ -1,4 +1,12 @@
 // Service worker entry point
-// Message routing and orchestration implemented in Task 13.1
+// Registers side panel and handles extension icon click
 
-export {};
+// Open side panel when extension icon is clicked
+chrome.action.onClicked.addListener((tab) => {
+  if (tab.id) {
+    chrome.sidePanel.open({ tabId: tab.id });
+  }
+});
+
+// Enable side panel to be openable
+chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
